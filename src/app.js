8f9ec401529d2a3324620f4505511dd885ffea03
ss707494 <ss707494@163.com>
2017-06-09 11:35:25 +0800

    爬虫

--- src/app.js
+++ src/app.js
@@ -2,6 +2,89 @@
  * Created by Administrator on 2017/6/2.
  */
 
+try {
+    var Spooky = require('spooky');
+    debugger
+} catch (e) {
+    var Spooky = require('../lib/spooky');
+}
+
+var spooky = new Spooky({
+    child: {
+        transport: 'http'
+    },
+    casper: {
+        logLevel: 'debug',
+        verbose: true
+    }
+}, function (err) {
+    if (err) {
+        e = new Error('Failed to initialize SpookyJS');
+        e.details = err;
+        throw e;
+    }
+
+    spooky.start(
+        'http://www.baidu.com');
+    spooky.then(function () {
+        this.emit('hello', 'Hello, from ' + this.evaluate(function () {
+                return document.title;
+            }));
+    });
+    spooky.run();
+});
+
+spooky.on('error', function (e, stack) {
+    console.error(e);
+
+    if (stack) {
+        console.log(stack);
+    }
+});
+
+/*
+ // Uncomment this block to see all of the things Casper has to say.
+ // There are a lot.
+ // He has opinions.
+ spooky.on('console', function (line) {
+ console.log(line);
+ });
+ */
+
+spooky.on('hello', function (greeting) {
+    console.log(greeting);
+});
+
+spooky.on('log', function (log) {
+    if (log.space === 'remote') {
+        console.log(log.message.replace(/ \- .*/, ''));
+    }
+});
+
+// const phantom = require('phantom');
+// (async function() {
+//     const instance = await phantom.create();
+//     const page = await instance.createPage();
+//
+//     // const status = await page.open('http://www.yac8.com/news/13937_2.html');
+//     const status = await page.open('http://www.baidu.com');
+//     await page.includeJs('./lib/jquery-3.2.1.min.js')
+//     var length = await page.evaluateAsync(function(){
+//         var $a = $('a[name="tj_trhao123"]');
+//         $a.trigger('click');
+//         window.location.href = 'https://www.hao123.com/?tn=96635944_hao_pg';
+//         return document;
+//     });
+//     console.log(status);
+//     const content = await page.property('content');
+//     console.log(content);
+//     debugger
+//
+//     await page.render('baidu.jpg');
+//
+//     await instance.exit();
+// }());
+
 var superagent = require('superagent');    //引入我们安装好的模块
 var request = require("request");
 var cheerio = require('cheerio');
@@ -22,10 +105,11 @@ async function getData(src, flag) {
             for(var i=0; i<imgArr.length; i++){
                 var _url = imgArr[i].indexOf('http:') === -1 ? 'http:' + imgArr[i] : imgArr[i];
                 // downloadImg(_url, imgArr[i].split('/')[4]);      //下载数组里的每张图片
-                urlData += _url + '\n';
+                urlData += _url + ',';
             }
             if (flag) {
-                console.log(urlData);
+                fs.writeFile('./test.txt', urlData);
+                // console.log(urlData);
             }
         })
 }
@@ -41,5 +125,5 @@ const _genArr = (arr = []) => n => !n
     ? arr : _genArr([n - 1, ...arr])(n - 1);
 
 const n = 90;
-_genArr()(n).map((e,i) => getData('http://jandan.net/ooxx/page-' + e, e == n-1))
+// _genArr()(n).map((e,i) => getData('http://jandan.net/ooxx/page-' + e, e == n-1))
 // getData('http://jandan.net/ooxx/page-1');
